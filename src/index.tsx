5081e91184abdbc404b523ff1986763a81418ff2
daniel-kalocsay <kdani782@gmail.com>
2020-03-16 12:51:03 +0100

    installed and configured firebase, added firestore

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase";
+
+
+// Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyBG8vXEMkwVY9vIHhmxoQ5YuW4kOOV-UHA",
+    authDomain: "dnd-session-organizer.firebaseapp.com",
+    databaseURL: "https://dnd-session-organizer.firebaseio.com",
+    projectId: "dnd-session-organizer",
+    storageBucket: "dnd-session-organizer.appspot.com",
+    messagingSenderId: "956829420098",
+    appId: "1:956829420098:web:29a5e0ad60d54eb7b23bdb",
+    measurementId: "G-24RB0Q38DL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
