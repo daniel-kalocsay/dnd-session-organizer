a1afa7d3f75a50087839915645dd9f4b8f619415
daniel-kalocsay <kdani782@gmail.com>
2020-03-16 11:35:20 +0100

    added components dir and empty LogIn component

---
+++ src/components/user/LogIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LogIn = () => {
+    return (
+        <div>
+            {/*//TODO handle user login with firebase*/}
+        </div>
+    )
+};
+
+export default LogIn;
