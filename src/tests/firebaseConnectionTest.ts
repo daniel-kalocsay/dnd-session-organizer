ebb90b17bfbada33b6256ff51f3f74f2b650faba
daniel-kalocsay <kdani782@gmail.com>
2020-03-18 14:03:55 +0100

    replaced firestore with realtime db

--- src/tests/firebaseConnectionTest.ts
+++ src/tests/firebaseConnectionTest.ts
@@ -1,32 +1,30 @@
-import { QuerySnapshot, DocumentSnapshot } from '@firebase/firestore-types';
-
 //TODO resolve warnings for import
-
 const firebaseConnectionTest = () => {
 
     const firebase = require("firebase");
     // Required for side-effects
     require("firebase/firestore");
 
+    let db = firebase.database().ref();
+    console.log(db);
+    
+    // firebase listener
 
-    let db = firebase.firestore();
-    let testCollectionRef = db.collection("test");
-
-    testCollectionRef
-        .onSnapshot((snapshot: QuerySnapshot) => {
-
-            //snapshot of the db collection's current state
-            if (snapshot) {
-                let snapDate = new Date();
-                let timestamp = snapDate.getHours() + ":" + snapDate.getMinutes() + ":" + snapDate.getSeconds();
-
-                console.log(`Entries in Test collection snapshot at ${timestamp}:`);
-
-                snapshot.forEach((entry: DocumentSnapshot) => {
-                    console.log(entry.data());
-                });
-            }
-        });
+    // testCollectionRef
+    //     .onSnapshot((snapshot: QuerySnapshot) => {
+    //
+    //         //snapshot of the db collection's current state
+    //         if (snapshot) {
+    //             let snapDate = new Date();
+    //             let timestamp = snapDate.getHours() + ":" + snapDate.getMinutes() + ":" + snapDate.getSeconds();
+    //
+    //             console.log(`Entries in Test collection snapshot at ${timestamp}:`);
+    //
+    //             snapshot.forEach((entry: DocumentSnapshot) => {
+    //                 console.log(entry.data());
+    //             });
+    //         }
+    //     });
 };
 
 export default firebaseConnectionTest;
