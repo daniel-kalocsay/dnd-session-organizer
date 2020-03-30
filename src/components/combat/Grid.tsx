0329b84c220cdf9f26247d94d95708d416f6bcfb
Attila Surányi <attila.suranyi@protonmail.com>
2020-03-30 11:54:06 +0200

    players field in Grid

--- src/components/combat/Grid.tsx
+++ src/components/combat/Grid.tsx
@@ -4,12 +4,14 @@ class Square {
 
 export class Grid {
     public tiles: Square[];
+    public players: string[];
 
-    constructor( size: 100 | 400 | 900 ) {
+    constructor( size: 100 | 400 | 900, players: string[] ) {
         let tiles = []
         for (let i= 1; i<size; i++) {
             tiles.push(new Square())
         }
         this.tiles = tiles;
+        this.players = players;
     }
 }
