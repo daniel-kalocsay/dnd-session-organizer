5bd6b1f26914f2987ec4061376eafcddfee0835c
daniel-kalocsay <kdani782@gmail.com>
2020-04-16 11:41:18 +0200

    user duplication in new campaign fixed, campaigns sorted by date after query

--- src/components/ui/CampaignList.tsx
+++ src/components/ui/CampaignList.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect, useContext} from "react";
+import {Link} from "react-router-dom";
 
 import firebase from "firebase";
-import { FirebaseContext } from "../contexts/FirebaseContext";
-import { useAuthState } from "react-firebase-hooks/auth";
+import {FirebaseContext} from "../contexts/FirebaseContext";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -16,118 +16,124 @@ type QuerySnapshot = firebase.firestore.QuerySnapshot;
 type DocumentSnapshot = firebase.firestore.DocumentSnapshot;
 
 const CampaignList = () => {
-  const sessionsRef = useContext(FirebaseContext)!.sessionsRef;
+    const sessionsRef = useContext(FirebaseContext)!.sessionsRef;
 
-  const campaignsRef = useContext(FirebaseContext)!.campaignsRef;
-  const usersRef = useContext(FirebaseContext)!.usersRef;
-  const [campaigns, setCampaigns] = useState([] as CampaignPreviewData[]);
+    const campaignsRef = useContext(FirebaseContext)!.campaignsRef;
+    const usersRef = useContext(FirebaseContext)!.usersRef;
+    const [campaigns, setCampaigns] = useState([] as CampaignPreviewData[]);
 
-  const auth = useContext(FirebaseContext)!.auth;
-  const [user, initializing, authError] = useAuthState(auth);
+    const auth = useContext(FirebaseContext)!.auth;
+    const [user, initializing, authError] = useAuthState(auth);
 
-  useEffect(() => {
-    if (user) {
-      fetchCampaigns();
-    }
-  }, [user]);
+    useEffect(() => {
+        if (user) {
+            fetchCampaigns();
+        }
+    }, [user]);
 
-  const fetchCampaigns = () => {
-    usersRef
-      .doc(user!.uid)
-      .collection("campaigns")
-      .get()
-      .then((querySnapshot: QuerySnapshot) => {
-        querySnapshot.forEach((campaignRef: DocumentSnapshot) => {
-          campaignsRef
-            .doc(campaignRef.id)
-            .get()
-            .then((campaignDoc: DocumentSnapshot) => {
-              if (campaignDoc.exists) {
-                let entry = campaignDoc.data();
-                let data = new CampaignPreviewData(
-                  campaignDoc.id,
-                  entry!.name,
-                  entry!.created_at.toDate()
-                );
-                setCampaigns((oldData) => [...oldData, data]);
-              }
-            });
-        });
-      });
-  };
+    const sortCampaignList = () => {
+        return campaigns.sort(
+            (a, b) => (a.createdAt < b.createdAt ? 1 : -1)
+        );
+    };
 
-  //TODO im sure this can be done in a better way
-  const deleteCampaign = (campaignId: string) => {
-    campaignsRef
-      .doc(campaignId)
-      .collection("players")
-      .get()
-      .then((playersDoc: QuerySnapshot) => {
-        playersDoc.forEach((player: DocumentSnapshot) => {
-          usersRef
-            .doc(player.id)
+    const fetchCampaigns = () => {
+        usersRef
+            .doc(user!.uid)
             .collection("campaigns")
+            .get()
+            .then((querySnapshot: QuerySnapshot) => {
+                querySnapshot.forEach((campaignRef: DocumentSnapshot) => {
+                    campaignsRef
+                        .doc(campaignRef.id)
+                        .get()
+                        .then((campaignDoc: DocumentSnapshot) => {
+                            if (campaignDoc.exists) {
+                                let entry = campaignDoc.data();
+                                let data = new CampaignPreviewData(
+                                    campaignDoc.id,
+                                    entry!.name,
+                                    entry!.created_at.toDate()
+                                );
+                                setCampaigns((oldData) => [...oldData, data]);
+                            }
+                        });
+                });
+                sortCampaignList();
+            });
+    };
+
+    //TODO im sure this can be done in a better way
+    const deleteCampaign = (campaignId: string) => {
+        campaignsRef
             .doc(campaignId)
-            .delete();
-          player.ref.delete();
+            .collection("players")
+            .get()
+            .then((playersDoc: QuerySnapshot) => {
+                playersDoc.forEach((player: DocumentSnapshot) => {
+                    usersRef
+                        .doc(player.id)
+                        .collection("campaigns")
+                        .doc(campaignId)
+                        .delete();
+                    player.ref.delete();
 
-          //TODO entire campaigns collection of the user is deleted after this
-          // if it was the only one. is that okay?
-        });
+                    //TODO entire campaigns collection of the user is deleted after this
+                    // if it was the only one. is that okay?
+                });
 
-        campaignsRef.doc(campaignId).delete();
-      });
+                campaignsRef.doc(campaignId).delete();
+            });
 
-    usersRef.doc(user!.uid).collection("campaigns").doc(campaignId).delete();
+        usersRef.doc(user!.uid).collection("campaigns").doc(campaignId).delete();
 
-    let updatedCampaigns = campaigns.filter(
-      (campaign: CampaignPreviewData) => campaign.uid !== campaignId
-    );
-    setCampaigns(updatedCampaigns);
-  };
+        let updatedCampaigns = campaigns.filter(
+            (campaign: CampaignPreviewData) => campaign.uid !== campaignId
+        );
+        setCampaigns(updatedCampaigns);
+    };
 
-  return (
-    <div style={styles.campaignListContainer}>
-      {campaigns.map((campaign: CampaignPreviewData) => (
-        <div style={styles.cardContainer} key={campaign.uid}>
-          <Card style={styles.card}>
-            <CardHeader title={campaign.name} />
-            <CardContent>
-              <p>
-                created at:{" "}
-                {campaign.createdAt ? campaign.getDate() : "no date"}
-              </p>
+    return (
+        <div style={styles.campaignListContainer}>
+            {sortCampaignList().map((campaign: CampaignPreviewData) => (
+                <div style={styles.cardContainer} key={campaign.uid}>
+                    <Card style={styles.card}>
+                        <CardHeader title={campaign.name}/>
+                        <CardContent>
+                            <p>
+                                Created: {campaign.createdAt ? campaign.getDate() : "unknown"}
+                            </p>
 
-              <Link to={`/campaign?id=${campaign.uid}`}>Details</Link>
-              <Button
-                onClick={() => {
-                  deleteCampaign(campaign.uid);
-                }}
-              >
-                Delete
-              </Button>
-            </CardContent>
-          </Card>
+                            <Link to={`/campaign?id=${campaign.uid}`}>Details</Link>
+                            <Button
+                                onClick={() => {
+                                    deleteCampaign(campaign.uid);
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default CampaignList;
 
 const styles = {
-  campaignListContainer: {
-    display: "grid",
-    gridGap: "1em",
-    gridTemplateColumns: "repeat(auto-fill, minmax(100px, 300px))",
-    gridTemplateRows: "repeat(auto-fill, 1fr)",
-  },
-  cardContainer: {
-    // maxWidth: 350,
-    // cursor: "pointer"
-  },
-  card: {
-    backgroundColor: "#eee",
-  },
+    campaignListContainer: {
+        display: "grid",
+        gridGap: "1em",
+        gridTemplateColumns: "repeat(auto-fill, minmax(100px, 300px))",
+        gridTemplateRows: "repeat(auto-fill, 1fr)",
+    },
+    cardContainer: {
+        // maxWidth: 350,
+        // cursor: "pointer"
+    },
+    card: {
+        backgroundColor: "#eee",
+    },
 };
--- src/components/ui/NewCampaign.tsx
+++ src/components/ui/NewCampaign.tsx
@@ -1,98 +1,98 @@
-import React, { useContext, useState, useEffect } from "react";
-import { FirebaseContext } from "../contexts/FirebaseContext";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, {useContext, useState, useEffect} from "react";
+import {FirebaseContext} from "../contexts/FirebaseContext";
+import {useAuthState} from "react-firebase-hooks/auth";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import UserSearch from "../user/UserSearch";
 import UserInfo from "../../model/UserInfo";
-import { firestore } from "firebase";
+import {firestore} from "firebase";
 
 const NewCampaign = () => {
-  const campaignsRef = useContext(FirebaseContext)!.campaignsRef;
-  const usersRef = useContext(FirebaseContext)!.usersRef;
-  const firebase = useContext(FirebaseContext)!.firebase;
+    const campaignsRef = useContext(FirebaseContext)!.campaignsRef;
+    const usersRef = useContext(FirebaseContext)!.usersRef;
+    const firebase = useContext(FirebaseContext)!.firebase;
 
-  const auth = useContext(FirebaseContext)!.auth;
-  const [user, initializing, authError] = useAuthState(auth);
+    const auth = useContext(FirebaseContext)!.auth;
+    const [user, initializing, authError] = useAuthState(auth);
 
-  const [players, setPlayers] = useState<UserInfo[]>([]);
+    const [players, setPlayers] = useState<UserInfo[]>([]);
 
-  useEffect(() => {
-    let userData = new UserInfo(user!.uid, user!.email!);
-    setPlayers([...players, userData]);
-  }, []);
+    useEffect(() => {
+        let userData = new UserInfo(user!.uid, user!.email!);
+        setPlayers([...players, userData]);
+    }, []);
 
-  const addCampaignIdToPlayers = (campaignId: string) => {
-    players.forEach((player: UserInfo) => {
-      usersRef.doc(player.uid!).collection("campaigns").doc(campaignId).set({});
-    });
-  };
-
-  const addPlayerIdsToCampaign = (campaignId: string, session: Object) => {
-    let batch: firestore.WriteBatch = firebase.firestore().batch();
-    let sessionRef = campaignsRef.doc(campaignId).collection("players");
-
-    players.forEach((player: UserInfo) => {
-      batch.set(sessionRef.doc(player.uid!), {});
-    });
-
-    campaignsRef
-      .doc(campaignId)
-      .set(Object.assign({}, session))
-      .then(() => {
-        batch.commit();
-      });
-  };
-
-  const createNewSession = (event: any) => {
-    event.preventDefault();
-
-    let sessionName = event.target.sessionName.value;
-    let session = {
-      name: sessionName,
-      created_at: firebase.firestore.FieldValue.serverTimestamp(),
-      created_by: user!.uid,
+    const addCampaignIdToPlayers = (campaignId: string) => {
+        players.forEach((player: UserInfo) => {
+            usersRef.doc(player.uid!).collection("campaigns").doc(campaignId).set({});
+        });
     };
 
-    let generatedCampaignId = usersRef
-      .doc(user!.uid)
-      .collection("campaigns")
-      .doc().id;
+    const addPlayerIdsToCampaign = (campaignId: string, session: Object) => {
+        let batch: firestore.WriteBatch = firebase.firestore().batch();
+        let playersCol = campaignsRef.doc(campaignId).collection("players");
 
-    addCampaignIdToPlayers(generatedCampaignId);
-    addPlayerIdsToCampaign(generatedCampaignId, session);
-  };
+        players.forEach((player: UserInfo) => {
+            batch.set(playersCol.doc(player.uid!), {});
+        });
 
-  const addPlayer = (player: UserInfo) => {
-    if (!players.includes(player)) {
-      let newPlayerList = [...players, player] as UserInfo[];
-      setPlayers(newPlayerList);
-    }
-  };
+        campaignsRef
+            .doc(campaignId)
+            .set(Object.assign({}, session))
+            .then(() => {
+                batch.commit();
+            });
+    };
 
-  return (
-    <div id="add-new-session">
-      <h3>Create new session:</h3>
-      {/* <NewCombatfield/> */}
-      <form onSubmit={createNewSession}>
-        <TextField
-          id="sessionName"
-          name="sessionName"
-          label="Session name"
-          required
-        />
-        <h3>Add player to session:</h3>
-        <UserSearch onAddPlayer={addPlayer} />
-        <Button type={"submit"} variant={"contained"} color="primary">
-          Create new session
-        </Button>
-      </form>
-      <h3>Players in the session:</h3>
-      {players.map((player) => (
-        <p>{player.name}</p>
-      ))}
-    </div>
-  );
+    const createNewCampaign = (event: any) => {
+        event.preventDefault();
+
+        let campaignName = event.target.campaignName.value;
+        let campaign = {
+            name: campaignName,
+            created_at: firebase.firestore.FieldValue.serverTimestamp(),
+            created_by: user!.uid,
+        };
+
+        let generatedCampaignId = usersRef
+            .doc(user!.uid)
+            .collection("campaigns")
+            .doc().id;
+
+        addCampaignIdToPlayers(generatedCampaignId);
+        addPlayerIdsToCampaign(generatedCampaignId, campaign);
+    };
+
+    const addPlayer = (player: UserInfo) => {
+        if (!players.some(p => p.uid === player.uid)) {
+            let newPlayerList = [...players, player] as UserInfo[];
+            setPlayers(newPlayerList);
+        }
+    };
+
+    return (
+        <div id="add-new-campaign">
+            <h3>Create new campaign:</h3>
+            {/* <NewCombatfield/> */}
+            <form onSubmit={createNewCampaign}>
+                <TextField
+                    id="sessionName"
+                    name="campaignName"
+                    label="Campaign name"
+                    required
+                />
+                <h3>Add player to campaign:</h3>
+                <UserSearch onAddPlayer={addPlayer}/>
+                <Button type={"submit"} variant={"contained"} color="primary">
+                    Create new campaign
+                </Button>
+            </form>
+            <h3>Players in the campaign:</h3>
+            {players.map((player) => (
+                <p>{player.name}</p>
+            ))}
+        </div>
+    );
 };
 
 export default NewCampaign;
