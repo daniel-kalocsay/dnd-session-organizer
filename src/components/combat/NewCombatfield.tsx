684315a3e5455956a228970644a3307eba9b9d44
daniel-kalocsay <kdani782@gmail.com>
2020-04-16 13:23:51 +0200

    reformatted usersearch with async await

--- src/components/combat/NewCombatfield.tsx
+++ src/components/combat/NewCombatfield.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
-import { FirebaseContext } from "../contexts/FirebaseContext";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, {useContext, useEffect, useState} from "react";
+import {FirebaseContext} from "../contexts/FirebaseContext";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 import Grid from "../../model/Grid";
 import UserInfo from "../../model/UserInfo";
@@ -12,70 +12,69 @@ import UserReference from "../user/UserReference";
 //TODO move component to a different directory, "combat" should only handle the combat itself
 
 const NewCombatfield = () => {
-  const combatfields = useContext(FirebaseContext)!.combatfieldsRef;
-  const users = useContext(FirebaseContext)!.usersRef;
+    const combatfields = useContext(FirebaseContext)!.combatfieldsRef;
+    const users = useContext(FirebaseContext)!.usersRef;
 
-  const auth = useContext(FirebaseContext)!.auth;
-  const [user, initializing, authError] = useAuthState(auth);
+    const auth = useContext(FirebaseContext)!.auth;
+    const [user, initializing, authError] = useAuthState(auth);
 
-  const [gridSize, setGridSize] = useState<number>(100);
-  const [players, setPlayers] = useState<UserInfo[]>([]);
+    const [gridSize, setGridSize] = useState<number>(100);
+    const [players, setPlayers] = useState<UserInfo[]>([]);
 
-  //TODO get the correct type for this event
-  const createNewCombatfield = (event: any) => {
-    event.preventDefault();
+    //TODO get the correct type for this event
+    const createNewCombatfield = (event: any) => {
+        event.preventDefault();
 
-    let combatfieldName = event.target.combatfieldName.value;
-    console.log(`trying to add ${combatfieldName} to db`);
+        let combatfieldName = event.target.combatfieldName.value;
+        console.log(`trying to add ${combatfieldName} to db`);
 
-    let playerIDs = players.map((player) => player.uid) as string[];
+        let playerIDs = players.map((player) => player.uid) as string[];
 
-    //TODO lazy creation of grid
-    // save grid with userID
-    let grid = new Grid(combatfieldName, gridSize, playerIDs);
-    let gridId = combatfields.doc().id;
-    combatfields.doc(gridId).set(Object.assign({}, grid));
+        //TODO save grid with userID
+        let grid = new Grid(combatfieldName, gridSize, playerIDs);
+        let gridId = combatfields.doc().id;
+        combatfields.doc(gridId).set(Object.assign({}, grid));
 
-    // save gridId to user
-    let userRecord = users.doc(user!.uid);
-    userRecord.collection("combatfields").add({ gridId });
-  };
+        // save gridId to user
+        let userRecord = users.doc(user!.uid);
+        userRecord.collection("combatfields").add({gridId});
+    };
 
-  const saveSize = (event: any, value: number | number[]) => {
-    let size = value as number;
-    setGridSize(size * 10);
-  };
+    const saveSize = (event: any, value: number | number[]) => {
+        let size = value as number;
+        setGridSize(size * 10);
+    };
 
-  const addPlayer = (player: UserInfo) => {
-    if (!players.some(p => p.uid === player.uid)) {
-      let newPlayerList = [...players, player] as UserInfo[];
-      setPlayers(newPlayerList);
-    }
-  };
+    const addPlayer = (player: UserInfo) => {
+        if (!players.some(p => p.uid === player.uid)) {
+            let newPlayerList = [...players, player] as UserInfo[];
+            setPlayers(newPlayerList);
+        }
+    };
 
-  useEffect(() => {
-    let currentUser = new UserInfo(user!.uid as string, "You");
-    setPlayers([currentUser]);
-  }, []);
+    useEffect(() => {
+        let currentUser = new UserInfo(user!.uid as string, "You");
+        setPlayers([currentUser]);
+    }, []);
 
-  return (
-    <div style={{ width: 400, height: 400 }}>
-      <GridSizeForm onSubmit={createNewCombatfield} saveSize={saveSize} />
+    return (
+        <div style={{width: 400, height: 400}}>
+            <GridSizeForm onSubmit={createNewCombatfield} saveSize={saveSize}/>
 
-      <h2>Add players to your combat field!</h2>
-      <UserSearch onAddPlayer={addPlayer} />
+            <h2>Add players to your combat field!</h2>
+            <UserSearch onAddPlayer={addPlayer}/>
 
-      <br />
-      <div>
-        <h3>Added players:</h3>
-        {players
-          ? players.map((player: UserInfo) => (
-              <UserReference key={player.uid!} userData={player} />
-            ))
-          : "No players added"}
-      </div>
-    </div>
-  );
+            <br/>
+            <div>
+                <h3>Added players:</h3>
+                {players
+                    ? players.map((player: UserInfo) => (
+                        <UserReference key={player.uid!} userData={player}/>
+                    ))
+                    : "No players added"}
+            </div>
+        </div>
+    );
 };
 
 export default NewCombatfield;
--- src/components/user/UserSearch.tsx
+++ src/components/user/UserSearch.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext} from "react";
 import firebase from "firebase";
-import { FirebaseContext } from "../contexts/FirebaseContext";
+import {FirebaseContext} from "../contexts/FirebaseContext";
 import UserInfo from "../../model/UserInfo";
 import UserList from "./UserList";
 
@@ -8,47 +8,48 @@ type QuerySnapshot = firebase.firestore.QuerySnapshot;
 type QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 
 const UserSearch = (props: any) => {
-  const usersDB = useContext(FirebaseContext)!.usersRef;
-  const [users, setUsers] = useState([] as UserInfo[]);
+    const usersDB = useContext(FirebaseContext)!.usersRef;
+    const [users, setUsers] = useState([] as UserInfo[]);
 
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    let searchValue = event.currentTarget.value;
+    const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+        let searchValue = event.currentTarget.value;
 
-    // clear results if value is empty, query new results otherwise
-    if (searchValue === "") {
-      setUsers([]);
-    } else {
-      searchUser(searchValue);
-    }
-  };
+        // clear results if value is empty, query new results otherwise
+        if (searchValue === "") {
+            setUsers([]);
+        } else {
+            searchUser(searchValue);
+        }
+    };
 
-  const searchUser = (username: string) => {
-    let usersFound = [] as UserInfo[];
+    const searchUser = async(username: string) => {
+        let usersFound = [] as UserInfo[];
 
-    usersDB
-      .orderBy("username")
-      .startAt(username)
-      .endAt(`${username}\uf8ff`)
-      .get()
-      .then((snapshot: QuerySnapshot) => {
-        snapshot.forEach((user: QueryDocumentSnapshot) => {
-          let uid = user.id;
-          let username = user.data().username;
-          let data = new UserInfo(uid, username);
-          usersFound.push(data);
+        let queriedUsers:QuerySnapshot = await
+            usersDB
+            .orderBy("username")
+            .startAt(username)
+            .endAt(`${username}\uf8ff`)
+            .get();
+
+        queriedUsers.forEach((user: QueryDocumentSnapshot) => {
+            let uid = user.id;
+            let username = user.data().username;
+            let data = new UserInfo(uid, username);
+            usersFound.push(data);
         });
-        setUsers(usersFound);
-      });
-  };
 
-  return (
-    <div>
-      <input placeholder={"Username"} onChange={handleChange} />
-      <div>
-        <UserList onUserClick={props.onAddPlayer} users={users} />
-      </div>
-    </div>
-  );
+        setUsers(usersFound);
+    };
+
+    return (
+        <div>
+            <input placeholder={"Username"} onChange={handleChange}/>
+            <div>
+                <UserList onUserClick={props.onAddPlayer} users={users}/>
+            </div>
+        </div>
+    );
 };
 
 export default UserSearch;
